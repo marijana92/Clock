dc086e5e28437fc5aedde1a2c794ff064a54eabe
Marijana <>
2014-10-01 11:59:17 +0200

    Dodati  java doc komentari i komentari u kodu u klasi BoardAnalog.

--- src/clock/BoardAnalog.java
+++ src/clock/BoardAnalog.java
@@ -30,22 +30,30 @@ public class BoardAnalog extends JPanel implements Runnable
     Date currentDate;
     int xcenter = 175, ycenter = 175;
     
+    /**
+     * Funkcija startuje izvrsavanje niti za klasu
+     * @see BoardAnalog.
+     */
     public void start() 
     {
         if (thread == null) 
         {
-            thread = new Thread(this);
-            thread.start();
+            thread = new Thread(this); //kreiramo nit
+            thread.start(); //startujemo izvrsavanje niti
         }
     }
 
+    /**
+     * Funkcija postavlja vrijednost niti
+     * @see #thread na null.
+     */
     public void stop() 
     {
         thread = null;
     }
     
     /**
-     *
+     * Dogadja koji se desava kad je pokrenuto izvrsavanje niti.
      */
     @Override
     public void run() 
@@ -54,43 +62,48 @@ public class BoardAnalog extends JPanel implements Runnable
         {
             try 
             {
-                Thread.sleep(1000);
+                Thread.sleep(1000); //stopiramo izvrsavanje programa za 1 sekundu
+                repaint(); //ponovno iscrtavanje, idemo na metodu paint
             } 
             catch (InterruptedException ex) 
             {
                 Logger.getLogger(BoardAnalog.class.getName()).log(Level.SEVERE, null, ex);
             }
-            
-            repaint();
         }
+        
         thread = null;
     }
     
-    @Override
-    public void update(Graphics g)
-    {
-        paint(g);
-    }
-    
+    /**
+     * Funkcija kreira osnovnu strukturu sata.
+     * Postavlja boju, crta oblik, i postvlja cetri broja, 3, 6, 9 i 12.
+     * 
+     * @param g objekat klase koji u sebi sadrzi funkcije za crtanje
+     */
     private void drawStructure(Graphics g)
     {
-        g.setFont(new Font("Kristen ITC", Font.PLAIN, 20));
-        g.setColor(Color.BLACK);
-        g.fillOval(xcenter - 150, ycenter - 150, 300, 300);
+        super.paint(g);
         
-        g.setColor(Color.RED);
-        g.setFont(new Font("Kristen ITC", Font.BOLD, 25));
-        g.drawString("12", xcenter - 10, ycenter - 130);
-        g.drawString("3", xcenter + 135, ycenter + 10);
-        g.drawString("6", xcenter - 10, ycenter + 145);
-        g.drawString("9", xcenter - 145, ycenter + 10);
+        g.setColor(Color.BLACK); //postavljamo boju pozadine
+        g.fillOval(xcenter - 150, ycenter - 150, 300, 300); //crtamo krug na poziciji 25, 25 sa duzinom i visinom 300
+        
+        g.setColor(Color.RED); //postavljamo boju sloba
+        g.setFont(new Font("Kristen ITC", Font.BOLD, 25)); //font slova
+        g.drawString("12", xcenter - 10, ycenter - 130); //ispisujemo 12
+        g.drawString("3", xcenter + 135, ycenter + 10);  //ispisujemo 3
+        g.drawString("6", xcenter - 10, ycenter + 145);  //ispisujemo 6
+        g.drawString("9", xcenter - 145, ycenter + 10);  //ispisujemo 9
     }
     
+    /**
+     * Funkcija na osnovu trenutnog vrmena racuna pozicije x i y koordinata za kazaljke 
+     * i sa tim koordinatama crta liniju.
+     * 
+     * @param g objekat klase koji u sebi sadrzi funkcije za crtanje
+     */
     @Override
     public void paint(Graphics g) 
     {
-        super.paint(g);
-
         int xhour, yhour, xminute, yminute, xsecond, ysecond, second, minute, hour;
         drawStructure(g);
 
@@ -99,12 +112,16 @@ public class BoardAnalog extends JPanel implements Runnable
         formatter.applyPattern("s"); //primjenjivanje formatera za sekunde
         second = Integer.parseInt(formatter.format(currentDate));
         
-        formatter.applyPattern("m");
+        formatter.applyPattern("m"); //primjenjivanje formatera za minute
         minute = Integer.parseInt(formatter.format(currentDate));
         
-        formatter.applyPattern("h");
+        formatter.applyPattern("h"); //primjenjivanje formatera za sate
         hour = Integer.parseInt(formatter.format(currentDate));
         
+        
+        /*
+        Racunanje x i y koordinata za kazaljke
+        */
         xsecond = (int) (Math.cos(second * 3.14f / 30 - 3.14f / 2) * 120 + xcenter);
         ysecond = (int) (Math.sin(second * 3.14f / 30 - 3.14f / 2) * 120 + ycenter);
         
@@ -116,14 +133,14 @@ public class BoardAnalog extends JPanel implements Runnable
         yhour = (int) (Math.sin((hour * 30 + minute / 2) * 3.14f / 180 - 3.14f
                 / 2) * 80 + ycenter);
         
-        g.setColor(Color.magenta);
+        g.setColor(Color.magenta); //boja sekundarice
         g.drawLine(xcenter, ycenter, xsecond, ysecond); //iscrtavanje sekundarice
 
-        g.setColor(Color.red);
-        g.drawLine(xcenter, ycenter - 1, xminute, yminute);
+        g.setColor(Color.red); //boja kazaljke za minute
+        g.drawLine(xcenter, ycenter - 1, xminute, yminute); //iscrtavanje kazaljke za minute
 
-        g.setColor(Color.green);
-        g.drawLine(xcenter, ycenter - 1, xhour, yhour);
+        g.setColor(Color.green); //boja kazaljke za sate
+        g.drawLine(xcenter, ycenter - 1, xhour, yhour); //iscrtavanje kazaljke za sate
 
         // Sinhronizovanje sa grafičkom kartom
         Toolkit.getDefaultToolkit().sync();
@@ -131,5 +148,4 @@ public class BoardAnalog extends JPanel implements Runnable
         // Optimizacija upotrebe RAM-a
         g.dispose();
     }
-    
 }
